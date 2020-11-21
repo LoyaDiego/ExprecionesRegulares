23ca4d82053a4e4b6d10b1f87ff3ff24e8cd513f
LoyaDiego <LoyaDiego@users.noreply.github.com>
2020-11-20 23:28:42 -0700

    PRACTICA1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -17,7 +17,7 @@ public class Controller {
     @FXML
     Label lblres;
     @FXML protected void initialize(){
-        listaejer.getItems().addAll("Ejercicio 1","Ejercicio 2");
+        listaejer.getItems().addAll("Ejercicio 1","Ejercicio 2","Validar no comienze con db","termine con count","dos nombres","validar MAC","Mayuscula y minuscula","binario de 8 digitos","Hexadecimal","Operacion de un digito","Numero de control","Validar Curp");
 
     }
 
@@ -27,11 +27,51 @@ public class Controller {
         String expresion="";
         switch(seleccion) {
             case 0: {
-               expresion=("^[^_][^p]([a-zA-z0-9]{3,})[q]$");
+               expresion=("^[_][^p]([a-zA-z0-9]{3,})[q]$");
                 break;
             }
             case 1:{
                 expresion=("[a-zA-z]+");
+                break;
+            }
+            case 2:{
+                expresion=("[^db][a-zA-Z]+");
+                break;
+            }
+            case 3:{
+                expresion=("[a-zA-Z]+(count)$");
+                break;
+            }
+            case 4:{
+                expresion=("([a-zA-Z]+)[: :]([a-zA-Z]+)");
+                break;
+            }
+            case 5:{
+                expresion=("(([a-zA-Z]+([1-9]))-([a-zA-Z]+([1-9]))-([a-zA-Z]+([1-9]))-([a-zA-Z]+([1-9]))-([a-zA-Z]+([1-9]))-([a-zA-Z]+([1-9])))|((([1-9])[a-zA-Z]+)-(([1-9])[a-zA-Z]+)-(([1-9])[a-zA-Z]+)-(([1-9])[a-zA-Z]+)-(([1-9])[a-zA-Z]+)-(([1-9])[a-zA-Z]+))|(([1-9][1-9])-([1-9][1-9])-([1-9][1-9])-([1-9][1-9])-([1-9][1-9])-([1-9][1-9]))");
+                break;
+            }
+            case 6:{
+                expresion=("(^([A-Z]))[a-zA-Z]+([A-Z])$");
+                break;
+            }
+            case 7:{
+                expresion=("[0-1][0-1][0-1][0-1][0-1][0-1][0-1][0-1]");
+                break;
+            }
+            case 8:{
+                expresion=("([0-9]+)|([a-fA-F]+)|(([0-9]+)([a-fA-F]+))|(([a-fA-F]+)([0-9]+))");
+                break;
+            }
+            case 9:{
+                expresion=("[0-9][+|-|*|/][0-9]");
+                break;
+            }
+            case 10:{
+                expresion= ("([0-9][0-9])(CG)([0-9]{1,4})");
+                break;
+            }
+            case 11:{
+                expresion=("[A-Z][AEIOU][A-Z][A-Z]([0-9]{6})[HM][A-Z][A-Z]([BCDFGHJKLMÑPQRSTVWXYZ]{3})([0-9]|[A-Z])[0-9]");
                 break;
             }
         }
